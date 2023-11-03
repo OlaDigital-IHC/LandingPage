a0abcb3181f38f7e8a45fc04af67f1a3685883b1
Eli7u7 <u20221f897@upc.edu.pe>
2023-11-03 00:14:42 -0500

    add menu java

--- js/menu.js
+++ js/menu.js
@@ -36,3 +36,51 @@ function prevSlide() {
         showSlide(currentIndex - 1);
     }
 }
+
+
+const gallery = document.querySelector("#gallery"); /*declaracion de variables con "const"*/ 
+/* MENU GALERIA*/
+gallery.addEventListener("click", (g) =>{
+    g.preventDefault();
+
+    const sectionGallery = document.querySelector(".gallery");
+    sectionGallery.scrollIntoView({behavior:"smooth"}); /*hace una transicion */
+})
+
+const testimonials = document.querySelector("#testimonials"); 
+/* MENU TESTIMONIALS*/
+testimonials.addEventListener("click", (t) =>{
+    t.preventDefault();
+
+    const sectionTestimonials = document.querySelector(".testimonials");
+    sectionTestimonials.scrollIntoView({behavior:"smooth"}); /*hace una transicion */
+})
+
+const questions = document.querySelector("#questions"); 
+/* MENU QUESTIONS*/
+questions.addEventListener("click", (q) =>{
+    q.preventDefault();
+
+    const sectionQuestions = document.querySelector(".questions");
+    sectionQuestions.scrollIntoView({behavior:"smooth"}); /*hace una transicion */
+})
+
+const team = document.querySelector("#team"); 
+/* MENU TEAM */
+team.addEventListener("click", (t) =>{
+    t.preventDefault();
+
+    const sectionTeam= document.querySelector(".team");
+    sectionTeam.scrollIntoView({behavior:"smooth"}); /*hace una transicion */
+})
+
+const Comentarios = document.querySelector("#Comentarios"); 
+/* MENU COMENTARIOS */
+Comentarios.addEventListener("click", (c) =>{
+    c.preventDefault();
+
+    const sectionComentarios= document.querySelector(".Comentarios");
+    sectionComentarios.scrollIntoView({behavior:"smooth"}); /*hace una transicion */
+})
+
+
